c5d6085b0b433be1ef47e5b82b2ea64e0ce3fc59
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2023-10-22 13:18:00 +0200

    Added initial frame
    Co-authored-by: Michael Ringhus Gertz <michael@ringhus.dk>
    Co-authored-by: jonfr <jojak19@student.sdu.dk>

---
+++ src/screens/CarDetails.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "../components/atoms/Text/Text";
+
+export default function CarDetails() {
+    return <View>
+        <Text>Car details!</Text>
+    </View>;
+}
